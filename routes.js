e3f82a24250ebb784274e7a1010a490336ab7127
Tyler Collard <tcollard@gmail.com>
2013-04-02 15:16:16 -0500

    Adding tests.

--- routes.js
+++ routes.js
@@ -8,7 +8,12 @@ var routes = function (server) {
         var controller = new GuestController(req.body);
 
         if(controller.isValid()) {
-            res.send(200);
+            controller.addGuest(function (err) {
+                if(typeof err !== 'undefined'){
+                    res.send(500, err);
+                }
+                res.send(200);
+            });
         }
 
         res.send(500, 'Incorrect parameters');
--- test/api-tests.js
+++ test/api-tests.js
@@ -12,13 +12,34 @@ var clear = function (done) {
 
 describe('API', function () {
     describe('/guest POST', function () {
-        it('responds with 500 when not posting valid parameters', function (done) {
+        it('should respond with 500 when not posting valid parameters', function (done) {
             request(server)
                 .post('/guest')
                 .expect(500, done);
         });
 
-        it('responds with 200 when passing valid first name, last name and email', function (done) {
+        it('should respond with 500 when passing validation but failing database entry', function (done) {
+            //enter the same guest twice into db
+            request(server)
+                .post('/guest')
+                .send({
+                    firstName: 'first',
+                    lastName: 'last',
+                    emailAddress: 'email@email.com'
+                });
+
+            request(server)
+                .post('/guest')
+                .send({
+                    firstName: 'first',
+                    lastName: 'last',
+                    emailAddress: 'email@email.com'
+                })
+                .expect(500, clear(done));
+
+        });
+
+        it('should respond with 200 when passing valid first name, last name and email', function (done) {
             request(server)
                 .post('/guest')
                 .send({

6d38a890fa973d3de35dcf86c1208c442f7e1c69
Tyler Collard <tcollard@gmail.com>
2013-05-15 19:12:30 -0500

    Switching over to npm package and adding back middleware for IE cors calls.

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ if(process.env.NODETIME_ACCOUNT_KEY) {
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config');
-var ie_cors = require('./lib/express-ie-cors');
+var cors = require('express-ie-cors');
 
 var server = express();
+server.use(cors());
 server.use(express.bodyParser());
-//server.use(ie_cors);
+
+
 var routes = require('./routes')(server);
 
 server.listen(config.port, function () {
